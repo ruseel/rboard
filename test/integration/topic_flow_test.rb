ce771e2acc58d6a7c241016dc9a13c877fbce8fb
JEONG, MunShik <ruseel@gmail.com>
2012-11-21 14:45:03 +0900

    remove hardcoded path

--- test/integration/topic_flow_test.rb
+++ test/integration/topic_flow_test.rb
@@ -2,26 +2,26 @@ require 'test_helper'
 
 class TopicFlowTest < ActionDispatch::IntegrationTest
   test "topic create, show, edit, update" do
-    get "topics/new"
+    get new_topic_path
 
-    post_via_redirect "topics", :topic => { subject: "one", body: "one_body" }
+    post_via_redirect topics_path, :topic => { subject: "one", body: "one_body" }
     assert_equal 0, path =~ /\/topics\/([\d]+)/
     id = $1
 
-    get "/topics/#{id}/edit"
+    get edit_topic_path(id)
     assert assigns(:topic)
 
-    put_via_redirect "/topics/#{id}", :topic => { subject: "modified", body: "modified body" }
-    assert_equal "/topics/#{id}", path
+    put_via_redirect topic_path(id), :topic => { subject: "modified", body: "modified body" }
+    assert_equal topic_path(id), path
   end
 
   test "topic create and add one comment" do
-    post_via_redirect "topics", :topic => { subject: "one", body: "one_body" }
+    post_via_redirect topics_path, :topic => { subject: "one", body: "one_body" }
     _path = path
     assert_equal 0, path =~ /\/topics\/([\d]+)/
     id = $1
 
-    post_via_redirect "comments", topic_id: id, comment: { body: "comment" }
+    post_via_redirect comments_path, topic_id: id, comment: { body: "comment" }
     assert_equal _path, path
   end
 end
